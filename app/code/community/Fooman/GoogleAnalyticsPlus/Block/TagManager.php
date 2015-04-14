85c7acfb0f57c2ccb6cd7745baecf2e4b9a904e2
rancey <tim.rance@corset-story.com>
2015-04-14 15:03:52 +0100

    Added local methods to TagManager.php

--- app/code/community/Fooman/GoogleAnalyticsPlus/Block/TagManager.php
+++ app/code/community/Fooman/GoogleAnalyticsPlus/Block/TagManager.php
@@ -12,6 +12,9 @@
 
 class Fooman_GoogleAnalyticsPlus_Block_TagManager extends Fooman_GoogleAnalyticsPlus_Block_Common_Abstract
 {
+    protected  $_quote;
+    protected  $_order;
+    
     protected function _construct()
     {
         parent::_construct();
@@ -40,5 +43,52 @@ class Fooman_GoogleAnalyticsPlus_Block_TagManager extends Fooman_GoogleAnalytics
     public function getTagManagerSnippet()
     {
         return Mage::getStoreConfig('google/analyticsplus_tagmanager/snippet');
+    }
+    
+    
+    /**
+     * helper to set the internal quote property
+     *
+     * @param $quote
+     */
+    public function setQuote($quote)
+    {
+        $this->_quote = $quote;
+    }
+
+ 
+    /**
+     * get conversion value, convert if chosen to a differnt currency
+     *
+     * @return int|string
+     */
+    public function getValue()
+    {
+        $order = $this->_getOrder();
+        if ($order) {
+            return Mage::helper('googleanalyticsplus')->convert($order, 'subtotal');
+        } else {
+            return 0;
+        }
+    }
+
+    /**
+     * get order from the last quote id
+     *
+     * @return mixed
+     */
+    protected function _getOrder()
+    {
+        if (!$this->_quote) {
+            $quoteId = Mage::getSingleton('checkout/session')->getLastQuoteId();
+            if ($quoteId) {
+                $this->_order = Mage::getModel('sales/order')->loadByAttribute('quote_id', $quoteId);
+            } else {
+                $this->_order = false;
+            }
+        } else {
+            $this->_order = Mage::getModel('sales/order')->loadByAttribute('quote_id', $this->_quote->getId());
+        }
+        return $this->_order;
     }
 }
