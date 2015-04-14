c2fc354157f96cd6d0624d0bd746173b9d958d64
rancey <tim.rance@corset-story.com>
2015-04-14 15:25:14 +0100

    Strip out unwanted code

--- app/code/community/Fooman/GoogleAnalyticsPlus/Block/TagManager.php
+++ app/code/community/Fooman/GoogleAnalyticsPlus/Block/TagManager.php
@@ -12,7 +12,6 @@
 
 class Fooman_GoogleAnalyticsPlus_Block_TagManager extends Fooman_GoogleAnalyticsPlus_Block_Common_Abstract
 {
-    protected  $_quote;
     protected  $_order;
     
     protected function _construct()
@@ -45,32 +44,6 @@ class Fooman_GoogleAnalyticsPlus_Block_TagManager extends Fooman_GoogleAnalytics
         return Mage::getStoreConfig('google/analyticsplus_tagmanager/snippet');
     }
     
-    
-    /**
-     * helper to set the internal quote property
-     *
-     * @param $quote
-     */
-    public function setQuote($quote)
-    {
-        $this->_quote = $quote;
-    }
-
- 
-    /**
-     * get conversion value, convert if chosen to a differnt currency
-     *
-     * @return int|string
-     */
-    public function getValue()
-    {
-        $order = $this->_getOrder();
-        if ($order) {
-            return Mage::helper('googleanalyticsplus')->convert($order, 'subtotal');
-        } else {
-            return 0;
-        }
-    }
 
     /**
      * get order from the last quote id
@@ -79,16 +52,14 @@ class Fooman_GoogleAnalyticsPlus_Block_TagManager extends Fooman_GoogleAnalytics
      */
     protected function _getOrder()
     {
-        if (!$this->_quote) {
-            $quoteId = Mage::getSingleton('checkout/session')->getLastQuoteId();
-            if ($quoteId) {
-                $this->_order = Mage::getModel('sales/order')->loadByAttribute('quote_id', $quoteId);
-            } else {
-                $this->_order = false;
-            }
-        } else {
-            $this->_order = Mage::getModel('sales/order')->loadByAttribute('quote_id', $this->_quote->getId());
-        }
+        
+		$quoteId = Mage::getSingleton('checkout/session')->getLastQuoteId();
+		if ($quoteId) {
+			$this->_order = Mage::getModel('sales/order')->loadByAttribute('quote_id', $quoteId);
+		} else {
+			$this->_order = false;
+		}
+        
         return $this->_order;
     }
 }
