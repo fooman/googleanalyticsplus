c1ea9ca0eb3e546e8c3936cb00d96ec8a071c04c
Joshua Estes <JoshuaEstes@users.noreply.github.com>
2017-06-13 12:48:20 -0400

    Issue 107 - Add Custom Dimensions

---
+++ app/code/community/Fooman/GoogleAnalyticsPlus/Model/System/Dimension.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Fooman GoogleAnalyticsPlus
+ *
+ * @package   Fooman_GoogleAnalyticsPlus
+ * @author    Kristof Ringleff <kristof@fooman.co.nz>
+ * @copyright Copyright (c) 2010 Fooman Limited (http://www.fooman.co.nz)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+class Fooman_GoogleAnalyticsPlus_Model_System_Dimension
+{
+
+    public function toOptionArray()
+    {
+        return array(
+            array('value' => null, 'label' => 'Disabled'),
+            array('value' => 'dimension1', 'label' => 'dimension1'),
+            array('value' => 'dimension2', 'label' => 'dimension2'),
+            array('value' => 'dimension3', 'label' => 'dimension3'),
+            array('value' => 'dimension4', 'label' => 'dimension4'),
+            array('value' => 'dimension5', 'label' => 'dimension5'),
+            array('value' => 'dimension6', 'label' => 'dimension6'),
+            array('value' => 'dimension7', 'label' => 'dimension7'),
+            array('value' => 'dimension8', 'label' => 'dimension8'),
+            array('value' => 'dimension9', 'label' => 'dimension9'),
+            array('value' => 'dimension10', 'label' => 'dimension10'),
+        );
+    }
+
+}
